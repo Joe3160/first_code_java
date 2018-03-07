e064b3b5955672965f35e4bdd28f4f658993c618
Joe3160 <694995669@qq.com>
2018-03-07 16:03:43 +0800

    使用链表

--- src/chapter3/lesson17/demo3118/Book.java
+++ src/chapter3/lesson17/demo3118/Book.java
@@ -4,4 +4,26 @@ public class Book {
     private String title;
     private double price;
 
+    public Book(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public boolean compare(Book book) {
+        if (book == null) {
+            return false;
+        }
+        if (this == book) {
+            return true;
+        }
+
+        if (this.title.equals(book.title) && this.price == book.price) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getInfo() {
+        return "图书名称："+title+"，图书价格"+price;
+    }
 }
